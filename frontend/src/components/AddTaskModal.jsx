1f14571779ad3fa1bd67116030426795a581b3ec
Dima <nik-dima07@mail.ru>
2026-08-08 16:45:54 +0400

    ADD(board): AddTaskModal element v1

---
+++ frontend/src/components/AddTaskModal.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+
+export default function AddTaskModal({ isOpen, onClose, onAddTask }) {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    if (!isOpen) return null;
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        if (!title.trim()) {
+            alert('Введите название задачи');
+            return;
+        }
+
+        const date = new Date().toISOString().split('T')[0];
+        const newTask = {
+            id: Date.now().toString(),
+            date: date, 
+            title: title.trim(),
+            description: description.trim() || 'Нет описания',
+            column: 'To Do' 
+        };
+
+        onAddTask(newTask);
+        
+        setTitle('');
+        setDescription('');
+        onClose();
+    };
+
+    const borderRuleStyle = "border-thirdary focus:ring-2 focus:ring-accent focus:border-transparent"
+    const textFieldClassName = `w-full px-4 py-2 border ${borderRuleStyle} rounded-lg focus:outline-none bg-primary`;
+
+    return (
+        <div 
+            className='fixed inset-0 bg-black/50 backdrop-blur-sm z-50 flex items-center justify-center p-4'
+            onClick={onClose}
+        >
+            <div
+                className='bg-secondary rounded-md '
+                onClick={(event) => event.stopPropagation()}
+            >
+                <div className='m-4 text-text'>
+                    <h3 className='flex  justify-center mb-4'>
+                        Добавление задачи
+                    </h3>
+
+                    <form onSubmit={handleSubmit}>
+                        <div className='mb-4'>
+                            <input
+                                type="text"
+                                value={title}
+                                onChange={(event) => setTitle(event.target.value)}
+                                placeholder="Введите название..."
+                                className={textFieldClassName}
+                                autoFocus
+                                required
+                            />
+                        </div>
+
+                        <div className='mb-4'>
+                            <textarea
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                                placeholder="Введите описание (необязательно)..."
+                                rows="3"
+                                className={textFieldClassName}
+                            />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
--- frontend/src/pages/Board.jsx
+++ frontend/src/pages/Board.jsx
@@ -11,8 +11,11 @@ import { useState } from 'react';
 
 import dragHandler from '../utils/dragHandler';
 import findTask from '../utils/findTask';
+
 import Column from "../components/Column";
 import Task from '../components/Task';
+import AddTaskModal from '../components/AddTaskModal';
+import Button from '../components/Button';
 
 import { COLUMN_TITLES, INITIAL_TASKS } from '../config';
 
@@ -20,6 +23,11 @@ import { COLUMN_TITLES, INITIAL_TASKS } from '../config';
 export default function Board() {
     const [tasks, setTasks] = useState(INITIAL_TASKS);
     const [activeId, setActiveId] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false); 
+
+    const handleAddTask = (newTask) => {
+        setTasks((prevTasks) => [...prevTasks, newTask]);
+    }
     
     const sensors = useSensors(
         useSensor(PointerSensor, {
@@ -39,25 +47,40 @@ export default function Board() {
     }
 
     return (
-        <DndContext {...dndContextProps}>
-            <div className="min-h-screen flex justify-between gap-6">
-                { COLUMN_TITLES.map((title) => (
-                    <Column
-                        key={title}
-                        title={title} 
-                        tasks={tasks.filter(task => task.column === title)} 
-                    />
-                ))}
-            </div>
-            <DragOverlay>
-                { activeTask 
-                    ? <Task 
-                        task={activeTask} 
-                        isMoving={true} 
-                    /> 
-                    : null 
-                }
-            </DragOverlay>
-        </DndContext>  
+        <>
+            <DndContext {...dndContextProps}>
+                <div className='justify-self-end mt-4 mb-4'>
+                    <Button
+                        onClick={() => setIsModalOpen(true)}
+                    > 
+                        Добавить задачу
+                    </Button>
+                </div>
+                <div className="min-h-screen flex justify-between gap-6">
+                    { COLUMN_TITLES.map((title) => (
+                        <Column
+                            key={title}
+                            title={title} 
+                            tasks={tasks.filter(task => task.column === title)} 
+                        />
+                    ))}
+                </div>
+                <DragOverlay>
+                    { activeTask 
+                        ? <Task 
+                            task={activeTask} 
+                            isMoving={true} 
+                        /> 
+                        : null 
+                    }
+                </DragOverlay>
+            </DndContext> 
+
+            <AddTaskModal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                onAddTask={handleAddTask}
+            />
+        </>
     );
 }
