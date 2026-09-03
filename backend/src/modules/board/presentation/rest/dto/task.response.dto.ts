9cd0ce7471696928187e87d69b055d3f2f3ad350
Dima <nik-dima07@mail.ru>
2026-09-03 18:49:40 +0300

    UPD(tasks): task dto

--- backend/src/modules/board/presentation/rest/dto/task.response.dto.ts
+++ backend/src/modules/board/presentation/rest/dto/task.response.dto.ts
@@ -2,7 +2,7 @@ export class TaskResponseDTO {
     constructor(
         private readonly id: number,
         private readonly title: string,
-        private readonly columnName: string,
+        private readonly columnId: number,
         private readonly description: string | null,
         private readonly createdAt: string,
     ) {}
--- backend/src/modules/board/presentation/rest/mappers/task.mapper.ts
+++ backend/src/modules/board/presentation/rest/mappers/task.mapper.ts
@@ -15,7 +15,7 @@ export function mapTaskToResponse(task: TaskData): TaskResponseDTO {
     return new TaskResponseDTO(
         task.id,
         task.title,
-        task.column.name,
+        task.column.id,
         task?.description,
         task.createdAt.toISOString(),
     );
